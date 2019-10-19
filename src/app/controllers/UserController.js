c0958e1029d9c52cf5fe8145c6eccbdd4d36b6ca
kielkow <matheuskiel@fiorifer.com.br>
2019-10-18 21:34:58 -0300

    Connection with db and create route create

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,17 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exist' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Monica Silva',
-    email: 'monica@fiorifer.com.br',
-    password_hash: '2',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
