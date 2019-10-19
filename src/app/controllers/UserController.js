ebd867efe64d0d1a69d7f024d0b78dcbd78bcfa2
kielkow <matheuskiel@fiorifer.com.br>
2019-10-18 21:53:04 -0300

    Fix request post

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,7 +2,7 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const userExists = User.findOne({ where: { email: req.body.email } });
+    const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exist' });
