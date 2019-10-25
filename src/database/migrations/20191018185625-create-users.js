0fcb8294933356e84f14bc6b69f9ed76433fd206
kielkow <matheuskiel@fiorifer.com.br>
2019-10-24 22:11:47 -0300

    Create Appointment model and migration

--- src/database/migrations/20191018185625-create-users.js
+++ src/database/migrations/20191018185625-create-users.js
@@ -5,7 +5,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
-        prymaryKey: true,
+        primaryKey: true,
       },
       name: {
         type: Sequelize.STRING,
