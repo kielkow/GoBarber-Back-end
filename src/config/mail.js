8f57b2995ac706ac05feadb26362cb1daa19f0f9
kielkow <matheuskiel@fiorifer.com.br>
2019-10-26 23:43:10 -0300

    Config NodeMailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5cce00db598247',
+    pass: 'ef287da6b2cf2b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
